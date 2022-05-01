6c4a9bea65b3374aa3dc47d9290ce1ef1c3e3ef8
Aleksandr Koshelev <ko4evneg@gmail.com>
2022-05-01 15:14:53 +0300

    - Fix pom tomcat-maven plugin scope - Add logging to servlet

--- src/main/java/com/github/ko4evneg/caloriesApp/web/UserServlet.java
+++ src/main/java/com/github/ko4evneg/caloriesApp/web/UserServlet.java
@@ -1,12 +1,17 @@
 package com.github.ko4evneg.caloriesApp.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.*;
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        log.info("redirect: users.jsp");
         response.sendRedirect("users.jsp");
     }
 
