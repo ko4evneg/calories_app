c02dee67556db1b121aec5b8c89c9648e4de8b6c
Aleksandr Koshelev <ko4evneg@gmail.com>
2022-05-22 02:55:07 +0300

    - Add JUnit 4 - Move SpringMain to test

--- src/main/java/com/github/ko4evneg/caloriesApp/web/UserServlet.java
+++ src/main/java/com/github/ko4evneg/caloriesApp/web/UserServlet.java
@@ -1,7 +1,7 @@
 package com.github.ko4evneg.caloriesApp.web;
 
-import com.github.ko4evneg.caloriesApp.SpringMain;
 import com.github.ko4evneg.caloriesApp.service.UserService;
+import com.github.ko4evneg.caloriesApp.util.ServletContextHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class UserServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
 
-    private final UserService service = SpringMain.getContext().getBean(UserService.class);
+    private final UserService service = ServletContextHolder.getContext().getBean(UserService.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
