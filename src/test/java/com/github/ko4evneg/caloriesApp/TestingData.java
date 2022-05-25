2944b293cc09bddde40095d55fbc2fae69e3b85f
ko4evneg <ko4evneg@gmail.com>
2022-05-25 18:01:29 +0300

    - Add MealTo hashCode and equals

--- src/test/java/com/github/ko4evneg/caloriesApp/TestingData.java
+++ src/test/java/com/github/ko4evneg/caloriesApp/TestingData.java
@@ -18,8 +18,13 @@ public class TestingData {
     public static final User user = new User(USER_ID, "User", "user@yandex.ru", "password", Role.USER);
     public static final User admin = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ADMIN);
 
+    public static final int MEAL_USER_ONE_ID = 1;
+    public static final int MEAL_USER_TWO_ID = 9;
+
+    public static final Meal singleMeal = new Meal(LocalDateTime.of(2022, Month.APRIL, 29, 10, 0), "Breakfast", 500, USER_ID);
+
     public static final List<Meal> meals = new ArrayList<>(List.of(
-            new Meal(LocalDateTime.of(2022, Month.APRIL, 29, 10, 0), "Breakfast", 500, USER_ID),
+            singleMeal,
             new Meal(LocalDateTime.of(2022, Month.APRIL, 29, 13, 0), "Dinner", 1000, USER_ID),
             new Meal(LocalDateTime.of(2022, Month.APRIL, 29, 20, 0), "Supper", 500, USER_ID),
             new Meal(LocalDateTime.of(2022, Month.APRIL, 30, 0, 0), "Edge case", 100, USER_ID),
